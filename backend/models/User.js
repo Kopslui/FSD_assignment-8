fa02564a86653ac5db22f945574f4eb7f7b0e9b5
Kopslui <113966349+Kopslui@users.noreply.github.com>
2024-11-10 19:07:54 +0530

    Add files via upload

---
+++ backend/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  fullname: { type: String, required: true },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+// Hash password before saving the user
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+
